0f526b690a4b3566033be5bc859f0f86127a3c8f
17etro <petrpetryk2016@gmail.com>
2020-12-02 14:02:14 +0200

    merged with old version

--- old/docs/js/main.js
+++ old/docs/js/main.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){
-    $('.slider2').slick({
-        autoplay: false,
-        infinite: true,
-        initialSlide: 1,
-        centerMode: true,
-        centerPadding: '25%',
-        arrows: false,
-        //easing: 'linear',
-        waitForAnimate: false,
-        responsive:[
-            {
-                breakpoint: 450,
-                settings:{
-                    centerPadding: '17%',
-                },
-            }
-        
-        ]
-    });
-});
+// $(document).ready(function(){
+//     $('.slider2').slick({
+//         autoplay: false,
+//         infinite: true,
+//         initialSlide: 1,
+//         centerMode: true,
+//         centerPadding: '25%',
+//         arrows: false,
+//         //easing: 'linear',
+//         waitForAnimate: false,
+//         responsive:[
+//             {
+//                 breakpoint: 450,
+//                 settings:{
+//                     centerPadding: '17%',
+//                 },
+//             }
+//
+//         ]
+//     });
+// });
 
 $('.section1__wheel').on('mousemove', (e) => {
     const x = e.pageX/$(window).width();
@@ -31,31 +31,32 @@ $('.section1__wheel').on('mousemove', (e) => {
 });
 
 $(document).ready(function () {
-    setTimeout(function () {
-        const
-            currentNiceSelect = document.querySelector(".nice-select .current"),
-            niceSelect = document.querySelector(".nice-select");
+    $('select').niceSelect();
 
-        const
-            oldOpen = document.createElement("div"),
-            newOpen = document.createElement("div"),
-            oldContactItem = document.createElement("div"),
-            newContactItem = document.createElement("div"),
-            oldMapParent = document.createElement("div"),
-            newMapParent = document.createElement("div");
+    const
+        currentNiceSelect = document.querySelector(".nice-select .current"),
+        niceSelect = document.querySelector(".nice-select");
 
-        oldOpen.classList.add("open");
-        newOpen.classList.add("open");
+    const
+        oldOpen = document.createElement("div"),
+        newOpen = document.createElement("div"),
+        oldContactItem = document.createElement("div"),
+        newContactItem = document.createElement("div"),
+        oldMapParent = document.createElement("div"),
+        newMapParent = document.createElement("div");
 
-        oldContactItem.classList.add("contact__item-text1");
-        newContactItem.classList.add("contact__item-text1");
+    oldOpen.classList.add("open");
+    newOpen.classList.add("open");
 
-        oldMapParent.classList.add("map");
-        newMapParent.classList.add("map");
-        oldMapParent.setAttribute("id", "parentMap");
-        newMapParent.setAttribute("id", "parentMap");
+    oldContactItem.classList.add("contact__item-text1");
+    newContactItem.classList.add("contact__item-text1");
 
-        oldOpen.innerHTML = `<div class="h1">Hours open 365 days</div>
+    oldMapParent.classList.add("map");
+    newMapParent.classList.add("map");
+    oldMapParent.setAttribute("id", "parentMap");
+    newMapParent.setAttribute("id", "parentMap");
+
+    oldOpen.innerHTML = `<div class="h1">Hours open 365 days</div>
 \t\t\t\t\t<div class="open__item">
 \t\t\t\t\t\t<div class="open__item-text day">Monday - Thursday:</div>
 \t\t\t\t\t\t<div class="open__item-text">10 am – 11 pm</div>
@@ -68,12 +69,12 @@ $(document).ready(function () {
 \t\t\t\t\t<div class="open__item">
 \t\t\t\t\t\t<div class="open__item-text day">Sunday:</div>
 \t\t\t\t\t\t<div class="open__item-text">10 am – 11 pm</div>
-\t\t\t\t\t</div><br>
+\t\t\t\t\t</div>
 \t\t\t\t\t<div class="open__item">
-\t\t\t\t\t\t<div class="open__item-text1">Last Ticket Sold One Hour Prior
-\t\t\t\t\t\t\t<br>to Closing</div>
+\t\t\t\t\t\t<div class="open__item-text1">Last ticket sold one hour prior
+\t\t\t\t\t\t\t<br>to closing</div>
 \t\t\t\t\t</div>`;
-        newOpen.innerHTML = `
+    newOpen.innerHTML = `
                 <div class="h1">Hours open 365 days</div>
 \t\t\t\t\t<div class="open__item">
 \t\t\t\t\t\t<div class="open__item-text day">Monday - Thursday:</div>
@@ -87,50 +88,144 @@ $(document).ready(function () {
 \t\t\t\t\t<div class="open__item">
 \t\t\t\t\t\t<div class="open__item-text day">Sunday:</div>
 \t\t\t\t\t\t<div class="open__item-text">10 am – 10 pm</div>
-\t\t\t\t\t</div><br>
+\t\t\t\t\t</div>
 \t\t\t\t\t<div class="open__item">
-\t\t\t\t\t\t<div class="open__item-text1">Last Ticket Sold One Hour Prior
-\t\t\t\t\t\t\t<br>to Closing</div>
+\t\t\t\t\t\t<div class="open__item-text1">Last ticket sold one hour prior
+\t\t\t\t\t\t\t<br>to closing</div>
 \t\t\t\t\t</div>`;
 
-        oldContactItem.innerHTML = `<a title="click to visit map" target="_blank" class="maplink" href="https://goo.gl/maps/tymxCCCDFqL2">St. Augustine, FL 32084</a>`;
-        newContactItem.innerHTML = `<a title="click to visit map" target="_blank" class="maplink" href="https://goo.gl/maps/4gJZzS48vH7bPs9s5">100 Saint George street, St. Augustine, Florida</a>`;
+    oldContactItem.innerHTML = `<a title="click to visit map" target="_blank" class="maplink" href="https://goo.gl/maps/11mRJZFuJsZhbc2K9">211 S State St, Chicago, IL<span>60604</span></a>`;
+    newContactItem.innerHTML = `<a title="click to visit map" target="_blank" class="maplink" href="https://goo.gl/maps/4gJZzS48vH7bPs9s5">100 St George St, St. Augustine, FL <span>32084</span></a>`;
 
-        oldMapParent.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55339.4777323848!2d-81.31243406949942!3d29.9012436946953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e6825775df7f4f%3A0xc2a183178b276027!2z0KHQtdC50L3Rgi3QntCz0LDRgdGC0ZbQvSwg0KTQu9C-0YDQuNC00LAsINCh0L_QvtC70YPRh9C10L3RliDQqNGC0LDRgtC4INCQ0LzQtdGA0LjQutC4!5e0!3m2!1suk!2sua!4v1596016481799!5m2!1suk!2sua" width="100%" height="340" frameborder="0" style="border:0;max-width:100%" allowfullscreen></iframe>`;
-        newMapParent.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3458.9461479293295!2d-81.31547958492514!3d29.894652282588197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e42795ef01c2bd%3A0x8753e82fcf3752db!2zMTAwIFN0IEdlb3JnZSBTdCwgU3QuIEF1Z3VzdGluZSwgRkwgMzIwODQsINCh0L_QvtC70YPRh9C10L3RliDQqNGC0LDRgtC4INCQ0LzQtdGA0LjQutC4!5e0!3m2!1suk!2sua!4v1596539385660!5m2!1suk!2sua" width="100%" height="340" frameborder="0" style="border:0;max-width:100%" allowfullscreen></iframe>`;
+    oldMapParent.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5941.280215846711!2d-87.627482!3d41.879089!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e2ca33f58ffd1%3A0xf06bc2d1f0d09729!2s211%20S%20State%20St%2C%20Chicago%2C%20IL%2060604%2C%20USA!5e0!3m2!1sen!2sua!4v1597659784049!5m2!1sen!2sua"  width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`;
+    newMapParent.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3458.9454919933796!2d-81.3152870848884!3d29.894671181936996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e42795ef799113%3A0x8f4b88d415750502!2sMedieval%20Torture%20Museum!5e0!3m2!1sen!2sua!4v1599847291706!5m2!1sen!2sua"  width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`;
 
-        niceSelect.onclick = function () {
-            const
-                parentContact = document.querySelector(".contact"),
-                currentItemAddress = document.querySelector(".contact .contact-us .contact__item.address .contact__item-text1"),
-                parentElementItemAddress = currentItemAddress.parentNode,
-                currentOpen = document.querySelector(".contact .open"),
-                parentParentMap = document.querySelector(".cell__line-footer"),
-                parentMap = document.querySelector("#parentMap");
+    let counter = 0;
 
+    function toggleCity (e) {
+        const
+            parentContact = document.querySelector(".contact"),
+            currentItemAddress = document.querySelector(".contact .contact-us .contact__item.address .contact__item-text1"),
+            parentElementItemAddress = currentItemAddress.parentNode,
+            currentOpen = document.querySelector(".contact .open"),
+            parentParentMap = document.querySelector(".cell__line-footer"),
+            parentMap = document.querySelector("#parentMap"),
+            preloader = document.querySelector(".lds-roller__wrap");
+
+        if (e.target.classList.contains("option") && counter > 0) {
+            preloader.classList.add("active");
+            document.querySelector("body").classList.add("no-scroll");
             setTimeout(() => {
-                if (currentNiceSelect.innerText === "St Augustine") {
-                    parentContact.removeChild(currentOpen);
-                    parentContact.appendChild(newOpen);
-
-                    parentElementItemAddress.removeChild(currentItemAddress);
-                    parentElementItemAddress.appendChild(newContactItem);
-
-                    parentParentMap.removeChild(parentMap);
-                    parentParentMap.appendChild(newMapParent);
-                } else if (currentNiceSelect.innerText === "Chicago") {
-                    parentContact.removeChild(currentOpen);
-                    parentContact.appendChild(oldOpen);
-
-                    parentElementItemAddress.removeChild(currentItemAddress);
-                    parentElementItemAddress.appendChild(oldContactItem);
-
-                    parentParentMap.removeChild(parentMap);
-                    parentParentMap.appendChild(oldMapParent);
-                }
-            }, 50);
+                preloader.classList.remove("active");
+                document.querySelector("body").classList.remove("no-scroll");
+            }, 1000);
         }
+
+        setTimeout(() => {
+            if (currentNiceSelect.innerText === "St Augustine") {
+                parentContact.removeChild(currentOpen);
+                parentContact.appendChild(newOpen);
+
+                parentElementItemAddress.removeChild(currentItemAddress);
+                parentElementItemAddress.appendChild(newContactItem);
+
+                parentParentMap.removeChild(parentMap);
+                parentParentMap.appendChild(newMapParent);
+
+                sessionStorage.setItem("selectCity", "St Augustine");
+            } else if (currentNiceSelect.innerText === "Chicago") {
+                parentContact.removeChild(currentOpen);
+                parentContact.appendChild(oldOpen);
+
+                parentElementItemAddress.removeChild(currentItemAddress);
+                parentElementItemAddress.appendChild(oldContactItem);
+
+                parentParentMap.removeChild(parentMap);
+                parentParentMap.appendChild(oldMapParent);
+
+                sessionStorage.setItem("selectCity", "Chicago");
+            }
+        }, 50);
+    }
+
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+
+            const
+                coordinatesChicago = {
+                    latitude: 41.879090,
+                    longitude: -87.627480
+                },
+                coordinatesAugustine = {
+                    latitude: 29.894616,
+                    longitude: -81.313250
+                },
+                distanceChicago = {
+                    latitude: position.coords.latitude - coordinatesChicago.latitude,
+                    longitude: position.coords.longitude - coordinatesChicago.longitude
+                },
+                distanceAugustine = {
+                    latitude: position.coords.latitude - coordinatesAugustine.latitude,
+                    longitude: position.coords.longitude - coordinatesAugustine.longitude
+                };
+
+            if (sessionStorage.getItem("selectCity")) {
+                const niceSelect = document.querySelector(".nice-select");
+                if (sessionStorage.getItem("selectCity") === "Chicago") {
+                    setTimeout(() => {
+                        niceSelect.querySelector('.option[data-value="1"]').click();
+                        niceSelect.classList.remove("open");
+                        counter++;
+                    }, 200);
+                } else {
+                    setTimeout(() => {
+                        niceSelect.querySelector('.option[data-value="2"]').click();
+                        niceSelect.classList.remove("open");
+                        counter++;
+                    }, 200);
+                }
+            } else if (distanceChicago.latitude > distanceAugustine.latitude || distanceChicago.longitude > distanceAugustine.longitude) {
+                const niceSelect = document.querySelector(".nice-select");
+                setTimeout(() => {
+                    niceSelect.querySelector('.option[data-value="2"]').click();
+                    niceSelect.classList.remove("open");
+                    counter++;
+                }, 200);
+            }
+
+        },
+
+        function(error){
+            if (sessionStorage.getItem("selectCity")) {
+                const selectCurrent = document.querySelector(".nice-select .current");
+                selectCurrent.innerText = sessionStorage.getItem("selectCity");
+                toggleCity();
+            }
+            counter++;
+        }
+    );
+
+    setTimeout(function () {
+        niceSelect.onclick = toggleCity;
     }, 100);
 
-    $('select').niceSelect();
+
+    var swiper = new Swiper('.slider2', {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+        loop: true,
+        centeredSlides: true,
+        slidesPerGroup: 1,
+        loopFillGroupWithBlank: true,
+        pagination: {
+            clickable: true,
+        },
+    });
+});
+$(window).load(function() {
+    const
+        sliderActive = document.querySelector(".swiper-slide-active"),
+        sliderContainer = document.querySelector(".swiper-container");
+
+    sliderContainer.style.height = sliderActive.offsetHeight + "px";
 });
